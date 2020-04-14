8c702eba4b170591a46741d3ab1cc748056613a2
Don Mallory <dmm333@gmail.com>
2020-04-14 22:35:16 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,6 +2,15 @@ def using_push(arr, string)
   arr.push(string)
 end
 
-def using_unshift(arr)
-  arr.unshift
+def using_unshift(arr, string)
+  arr.unshift(string)
 end
+
+def using_pop(arr)
+  arr.pop
+end
+
+def pop_with_args(arr, num)
+  arr.pop(num)
+end
+
